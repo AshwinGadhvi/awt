4c6adbd25b1b00b5f5743522243a2d957d55b19d
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-11 00:01:18 +0530

    Practical 1

---
+++ Practical_1.js
@@ -0,0 +1,19 @@
+let name = "Ashwin Gadhvi"; 
+let age = 25; 
+let isStudent = true;
+
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is a student:", isStudent);
+
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+
+let number1 = 10;
+let number2 = 20;
+let sum = addNumbers(number1, number2);
+console.log("Sum of", number1, "and", number2, "is:", sum);
