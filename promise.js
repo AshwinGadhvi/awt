34709854cbadfb421be86d99fbb0b1cb3142fcba
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-16 15:16:22 +0530

    Learning About Promise In JavaScript.

---
+++ promise.js
@@ -0,0 +1,20 @@
+// const { reject } = require("async");
+console.log("ashwin 1");
+let promise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let success = true;
+        if(success){
+            resolve("operation was successfully");
+        }
+        else{
+            reject("operation was failed");
+        }
+    },1000);
+});
+
+promise.then((result)=>{
+    console.log(result);
+})
+.catch((error)=>{
+    console.log(error);
+});
