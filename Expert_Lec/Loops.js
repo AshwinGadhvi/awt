6c6e76e6e35c8da53a0bb5cac83297e1f01fb3cd
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-18 10:57:28 +0530

    Learning All Loops.

---
+++ Expert_Lec/Loops.js
@@ -0,0 +1,23 @@
+const arr = [...Array(1000000).keys()];
+
+for (let i = 0; i < arr.length; i++) {
+}
+
+let j = 0;
+while (j < arr.length) {
+    j++;
+}
+
+let k = 0;
+do {
+    k++;
+} while (k<arr.length);
+
+arr.forEach(element=>{
+
+});
+
+//fastest loop is For Loop
+//ForEach Use For Multiple Time When We Have To Execute Again And Again
+
+//for wathing time we use time and timeEnd method
