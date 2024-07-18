4f10abb081fc2b6e174365857d51f995578a6231
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-07-18 10:56:34 +0530

    Using Spread Operator.

---
+++ Expert_Lec/Spread_Operator.js
@@ -0,0 +1,12 @@
+const data = [1,2,3,4,5,9];
+
+const [,,a] = data;
+
+console.log(a);
+console.log(data[0]);
+console.log(data[1]);
+
+//deleting data
+
+const [c,b,...rest] = data;
+console.log(rest);
