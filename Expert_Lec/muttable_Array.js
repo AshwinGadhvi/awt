92795f639a34b87eb97809dd6f6d43ea730ed58e
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-08-17 20:42:00 +0530

    Performing Mutuable Task For Crud In Javascript on Array.

--- Expert_Lec/muttable_Array.js
+++ Expert_Lec/muttable_Array.js
@@ -1,28 +1,28 @@
 const arr = [1,2,3,4];
 
-arr.push(6);
-console.log(arr);
+// arr.push(6);
+// console.log(arr);
 
-arr.pop();
-console.log(arr);
+// arr.pop();
+// console.log(arr);
 
-arr.unshift(0);
-console.log(arr);
+// arr.unshift(0);
+// console.log(arr);
 
-arr.shift();
-console.log(arr);
+// arr.shift();
+// console.log(arr);
 
 //add item
 const index = 3;
 arr.splice(index,0,8);
-console.log(arr);
+console.log("Using Spilice : "+arr);
 
 //update item
 const index2 = 2;
 arr.splice(index2,1,9);
-console.log(arr);
+console.log("Using Spilice : "+arr);
 
 //delete item
 const end = 3;
 arr.splice(end,2);
-console.log(arr);
+console.log("Using Spilice : "+arr);
