4bafbaaf8d14fc34eadbcc89412155d82c8474e2
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-08-17 20:40:45 +0530

    Making Server Using Javascript.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const http = require('http');
 
 //create server
-const server = http.createServer((req, res) => {
-    res.end("Hello From Server");
-}); 
-//listen on port 3000
-server.listen(3000,'127.0.0.1', () => {
-    console.log("Server is listening on port 3000");
-    });
+// const server = http.createServer((req, res) => {
+//     res.end("Hello From Server");
+// }); 
+// //listen on port 3000
+// server.listen(3000,'127.0.0.1', () => {
+//     console.log("Server is listening on port 3000");
+//     });
+
+
+const server2 = http.createServer((req,res)=>{
+    res.end("Nikkal Mc");
+});
+
+server2.listen(3000,'127.0.0.1',()=>{
+    console.log("Oi Huig")
+});
